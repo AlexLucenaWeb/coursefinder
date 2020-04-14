84becc4fab075a5d7ea7769fd173b7892d051197
carlalew <56562814+carlalew@users.noreply.github.com>
2020-04-14 22:10:11 +0100

    carla updates Tuesday

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require ('mongoose');
 const validator = require ('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -17,14 +18,39 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'please enter a password'],
-        minlength: 8
+        minlength: 8,
+        select: false
     },
     confirmPassword: {
         type: String,
         required: [true, 'you must confirm your password!'],
-
-    }
+        validate: {
+            validator: function(el) {
+                return el === this.password; 
+            },
+            message:'Passwords do no match!'
+        }
+     }
 });
+
+userSchema.pre('save', async function(next) {
+    //only run this function if password was changed
+    if(!this.isModified('password')) return next();
+
+    //hash password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // delete confirm password field
+    this.confirmPassword = undefined;
+    next();
+});
+
+userSchema.methods.correctPassword = async function(
+    candidatePassword, 
+    userPassword) 
+    {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('User', userSchema);
 
