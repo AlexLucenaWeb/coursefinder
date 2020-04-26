e675454d58c2f004285fd05ea58937340bad56d9
janoweb <janoweb@users.noreply.github.com>
2020-04-26 21:43:16 +0100

    apperror problem fixed

--- controllers/basicHandler.js
+++ controllers/basicHandler.js
@@ -1,5 +1,5 @@
 const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/apperror');
+const AppError = require('./../utils/appError');
 const APIFeatures = require('./../utils/apiFeatures');
 
 // Errors catch by catchAsync, error handler and  error class
